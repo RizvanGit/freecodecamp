87d0f15ef36abc7d20d3aedef8dda64a44d1a5cd
Rizvan <imeandmyep2@gmail.com>
2023-06-21 15:49:21 +0300

    Structure added

---
+++ react-fcc/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react'
+
+const Header: FC = () => {
+    return <h1>Freecodecamp React</h1>
+}
+
+export default Header
---
+++ react-fcc/src/components/lessons/task.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+
+const Task: FC = () => {
+  return (
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here is a subtitle</p>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Task
